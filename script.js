19da7c46b0aed99c48b962462999f8947563c920
teiqn <teikoresawa@gmail.com>
2023-03-28 20:58:45 +0900

    Added requirement for valid e-mail, phone, and pass1===pass2.

---
+++ script.js
@@ -0,0 +1,38 @@
+/"this checks for pass1 and pass2 if they're the same"/
+function validatePassword() {
+    var pass1 = document.getElementById("pass1").value;
+    var pass2 = document.getElementById("pass2").value;
+    if (pass1 != pass2) {
+      alert("Passwords do not match!");
+      return false;
+    }
+    return true;
+}
+
+/"this will require email field to be something@something.something"/
+function validateEmail() {
+    const emailInput = document.getElementById('email');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (!emailRegex.test(emailInput.value)) {
+    emailInput.setCustomValidity('Please enter a valid email address.');
+        return false;
+} else {
+    emailInput.setCustomValidity('');
+        return true;
+}
+}
+  
+document.getElementById('email').addEventListener('input', validateEmail);
+
+/"this will require phone field to be just numbers"/
+function validatePhone() {
+    const phoneInput = document.getElementById('phone');
+    const phoneValue = phoneInput.value;
+    if (!/^\d+$/.test(phoneValue)) {
+        phoneInput.setCustomValidity('Phone number can only contain numbers');
+        return false;
+    } else {
+        phoneInput.setCustomValidity('');
+        return true;
+}
+}
